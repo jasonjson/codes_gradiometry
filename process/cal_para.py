b86608a975e4a4e8d9b0ae5dbb42126de6ea3ceb
Yuanyuan Liu <johnny.lyy@gmail.com>
2014-11-25 13:49:46 -0500

    new files

--- process/cal_para.py
+++ process/cal_para.py
@@ -7,32 +7,29 @@ import numpy as np
 from os import system
 import re
 
+#calculate peak time window
 f_master = open('master_sta','r')
 master_info = f_master.read().split()
 master_tr = read(master_info[0])[0]
 peak_time = int(master_tr.data.argmax() * master_tr.stats.delta)
+#use the latitude of master station to find corresponding strain, amp and vel
 master_lat = master_info[3]
-
 f_loc = open('loc_sta','r') #location of stations within this folder
 lines = f_loc.readlines()
 st1 = read('shift.*.z') #read amplitude data
 st2 = read('vel.*.z') #read velocity data
-f_vel = open('vel.txt','w+') #we use 'w+' to add new lines to the same file
-f_ux_all = open('ux.txt','w+')
-f_uy_all = open('uy.txt','w+')
 Uzxx = []
 Uzyy = []
 Ux_y = []
 Vx_y = []
-#for t in arange(peak_time-100,peak_time+102,2):
-for t in arange(peak_time-100,peak_time-98,2):
+for t in arange(peak_time-100,peak_time+102,2):
     #find amplitude information
     amp_data = []
     for tr in st1:
         amp_data.append(tr.data[int(t/tr.stats.delta)]*1000000)
     per_error = max(amp_data) * 0.005 
     damping = (max(amp_data) * 0.04 / 6.371) ** 2
-    #produce GPS_raw.dat
+    #produce GPS_raw_x.dat and GPS_raw_y.dat
     f_raw_x = open('GPS_raw_x.dat','w')
     f_raw_y = open('GPS_raw_y.dat','w')
     f_raw_x.write('lon lat Ve Vn Se Sn Cen Site Ref \n')
@@ -44,21 +41,21 @@ for t in arange(peak_time-100,peak_time-98,2):
         i += 1  #update index for amplitude data in the list amp_data
     f_raw_x.close()
     f_raw_y.close()
-    #produce amplitude data file GPS_raw.dat
+    #produce Ux_y data 
     f_amp = open('GPS_raw_x.dat','r')
     for line in f_amp.readlines():
         if re.search(master_lat,line):
             Ux_y.append(float(line.split()[2])/1000000000)
     #produce Uzxx data
     system('mv GPS_raw_x.dat GPS_raw.dat')
-    system('./process_wg2 '+str(damping))
+    system('process_wg2 '+str(damping))
     f_strain_x = open('strain.out','r')
     for line in f_strain_x.readlines():
         if re.search('^[\d\s]*'+master_lat,line):
             Uzxx.append(float(line.split()[4])/1000000000000)
     #produce Uzyy data
     system('mv GPS_raw_y.dat GPS_raw.dat')
-    system('./process_wg2 '+str(damping))
+    system('process_wg2 '+str(damping))
     f_strain_y = open('strain.out','r')
     for line in f_strain_y.readlines():
         if re.search('^[\d\s]*'+master_lat,line):
--- process/main.sh
+++ process/main.sh
@@ -4,12 +4,13 @@ for ((i=1;i<=30;i++))
 do
 stationname=`awk -v a=$i '{x=$1; split(x,aa,".");if(NR==a) print aa[2]}' header_all`
 cd $stationname
-saclst stlo stla f TA.*.z > loc_sta
-cal_shift_peak.py
+saclst stlo stla f TA.*.z > loc_sta #produce station location data file
+awk '{print $1,$3,$4}' master_sta > loc_master  #produce loc_master for make_geometry_new2
+cal_shift_peak.py #calculate shifting time
 #shift waveforms
 awk '{print "r TA."$1".z";print "chnhdr b",$2; print "w shift."$1".z"}END{print "q"}' shift_time | sac
 #differente waveforms to get ground velocity
 awk '{print "r shift."$1".z";print "dif";print "w vel."$1".z"}END{print "q"}' shift_time | sac
-create_GPS_raw.py
+cal_para.py
 done
 
