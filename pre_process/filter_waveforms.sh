471eb7803a80e540257eaee9593fea357a1eb995
Yuanyuan Liu <johnny.lyy@gmail.com>
2014-11-24 22:16:19 -0500

    create velo_start for each period folder

--- pre_process/filter_waveforms.sh
+++ pre_process/filter_waveforms.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-for i in 20 25 30 35 40 45 50 55 60 65 70 75 80 90 100 110 120 130 140 150
+for i in 20 25 30 35 40 45 50 
 do
 	mkdir Period_$i
 	cp *.z geometry.dat header_all Period_$i
 	cd Period_$i
+	echo 3.8 > velo_start
 	lower_freq=`awk -v a=$i 'BEGIN{print (1/a)*0.9}'`
 	higher_freq=`awk -v a=$i 'BEGIN{print (1/a)*1.1}'`
-	cat header_all | awk -v x=$lower_freq -v y=$higher_freq '{print "r",$1; print "bp c "x,y" n 4 p 2"; print "w over"}END{print "q"}' |sac
+	awk -v x=$lower_freq -v y=$higher_freq '{print "r",$1; print "bp c "x,y" n 4 p 2"; print "w over"}END{print "q"}' header_all | sac
+	cd ..
+done
+
+for i in 55 60 65 70 75 80 90 100 110 120 130 140 150
+do
+	mkdir Period_$i
+	cp *.z geometry.dat header_all Period_$i
+	cd Period_$i
+	echo 4.0 > velo_start
+	lower_freq=`awk -v a=$i 'BEGIN{print (1/a)*0.9}'`
+	higher_freq=`awk -v a=$i 'BEGIN{print (1/a)*1.1}'`
+	awk -v x=$lower_freq -v y=$higher_freq '{print "r",$1; print "bp c "x,y" n 4 p 2"; print "w over"}END{print "q"}' header_all | sac
 	cd ..
 done
 #delete original data
--- process/cal_para.sh
+++ process/cal_para.sh
@@ -6,7 +6,9 @@ stationname=`awk -v a=$i '{x=$1; split(x,aa,".");if(NR==a) print aa[2]}' header_
 cd $stationname
 saclst stlo stla f TA.*.z > loc_sta
 cal_shift_peak.py
+#shift waveforms
 awk '{print "r TA."$1".z";print "chnhdr b",$2; print "w shift."$1".z"}END{print "q"}' shift_time | sac
+#differente waveforms to get ground velocity
 awk '{print "r shift."$1".z";print "dif";print "w vel."$1".z"}END{print "q"}' shift_time | sac
 create_GPS_raw.py
 done
