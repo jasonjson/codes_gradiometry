560bde5c33ed7dbb916fc19970c375532e93155b
Yuanyuan Liu <johnny.lyy@gmail.com>
2014-12-28 19:35:31 -0500

    remove stations with abnormal b values

--- remove_reponse_origtime.sh
+++ remove_reponse_origtime.sh
@@ -2,9 +2,6 @@
 
 #remove bad stations
 awk '{print "rm TA."$1"* SAC*TA_"$1"*"}' bad | sh  #stations far away from the main array
-saclst b f *.z | awk '{if($2>500 || $2<-500) print "rm "$1}' | sh  #stations with abnormal b values
-saclst dist stlo stla baz f *.z | awk '{print $1,$2,$3,$4,$5-180}' > header_all
-create_geometry.py
 
 #remove instrumental response and modify original time
 saclst nzyear nzjday nzhour nzmin nzsec  f TA.Q*.z > header.info
@@ -17,4 +14,7 @@ ls SAC* | awk '{x=$1;split(x,aa,"_");print "mv "$1,aa[1]"_"aa[4]}' | sh
 ls *.z | awk '{x=$1;split(x,aa,".");print $1,"SAC_"aa[2]}' | awk -v a=$jyear -v b=$jday -v c=$jhour -v d=$jmin -v e=$jsec '{x=$1;y=$2;print "r",x; print "rmean"; print "rtrend"; print "taper"; print "trans from polezero subtype",y,"to none freq 0.001 0.002 0.1 0.2";print "chnhdr o gmt "a,b,c,d,e; print "evaluate to tt1 &1,o * -1"; print "chnhdr allt %tt1"; print "w over"}END{print "q"}' |sac
 
 #delete useless files
+saclst b f *.z | awk '{if($2>500 || $2<-500) print "rm "$1}' | sh #stations with abnormal b values
+saclst dist stlo stla baz f *.z | awk '{print $1,$2,$3,$4,$5-180}' > header_all
+create_geometry.py
 rm SAC* bad header.info
