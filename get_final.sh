b96cc2683e706a0890e1087830243d9dbd46375c
Yuanyuan Liu <johnny.lyy@gmail.com>
2015-01-21 08:46:57 -0500

    no longer cal peak and travel time, only transfer back final_para

--- get_final.sh
+++ get_final.sh
@@ -58,17 +58,19 @@ cat azi_rad_geo_* > azi_rad_geo_all
 cal_stru_velo.sh
 
 #plot AxAy
-image_A
+#image_A
 
 #plot travel_time peak_amp
-image_time_amp
+#image_time_amp
 
 #plot dyna_velo stru_velo azi_var rad_patt geo_spread
-image_para
+#image_para
 
 #store all the figures
-mv *.ps ../plots
+#mv *.ps ../plots
 cd ..
 ### After everything is done, copy all of the results off of the SSD back to 
 ### where you submitted the job
-rsync -av $SCRATCH/ $PBS_O_WORKDIR/
+tar -cf $PBS_O_WORKDIR/ssdout.$PBS_JOBID.tar $SCRATCH/final_para 
+tar -xf ssdout.$PBS_JOBID.tar
+mv scratch/yuanliu/$PBS_JOBID/final_para .
