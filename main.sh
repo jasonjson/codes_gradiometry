3cd2a2d50e0a99c40475735901a489de641f5ece
Yuanyuan Liu <johnny.lyy@gmail.com>
2015-02-01 15:31:59 -0500

    deal with stations in CI BA series

--- main.sh
+++ main.sh
@@ -6,7 +6,7 @@ stationname=`awk -v a=$i '{x=$1; split(x,aa,".");if(NR==a) print aa[2]}' header_
 cd $stationname
 awk '{print $1,$3,$4}' master_sta > loc_master  #produce loc_master for make_geometry_new2
 cal_shift_$1.py #calculate shifting time
-awk '{print "r TA."$1".z";print "chnhdr b",$2; print "w shift."$1".z"}END{print "q"}' shift_time | sac 
+awk '{print "r [ABCNT]*"$1".z";print "chnhdr b",$2; print "w shift."$1".z"}END{print "q"}' shift_time | sac 
 awk '{print "r shift."$1".z";print "dif";print "w vel."$1".z"}END{print "q"}' shift_time | sac 
 cal_para.py #update phase velocity
 #store final results
