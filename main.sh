ee4929e8d2716d24a8f1141072a53c5f73381014
Yuanyuan Liu <johnny.lyy@gmail.com>
2015-02-02 18:16:53 -0500

    record bad stations

--- main.sh
+++ main.sh
@@ -9,6 +9,12 @@ cal_shift_$1.py #calculate shifting time
 awk '{print "r TA."$1".z";print "chnhdr b",$2; print "w shift."$1".z"}END{print "q"}' shift_time | sac 
 awk '{print "r shift."$1".z";print "dif";print "w vel."$1".z"}END{print "q"}' shift_time | sac 
 cal_para.py #update phase velocity
+#if calculation goes wrong for this station, write it down
+if [ ! -f pxpy.dat ]
+then
+	touch ../bad_stations
+	echo $stationname > ../bad_stations
+fi
 #store final results
 cp pxpy.dat pxpy_main #store new slowness for shifting and updating 
 mv pxpy.dat ../final_para/pxpy_$stationname
