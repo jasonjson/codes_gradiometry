d601e5001fba243b8580ab50eaa0b49944aaa6b9
Yuanyuan Liu <johnny.lyy@gmail.com>
2015-05-04 14:46:10 -0400

    update st.txt

--- cal_stru_velo.sh
+++ cal_stru_velo.sh
@@ -11,5 +11,5 @@ awk -v a=$percentage 'BEGIN {print "lon lat Ve Vn Se Sn Cen Site Ref"} {print $1
 process_AB $damping
 num_stations=`wc -l ../header_all | awk '{print $1}'`
 awk -v a=$num_stations '{if(NR>=4&&NR<=a+3)print $2,$3,$4/1000000,$5/1000000}' strain.out > lap_A
-# $1-dyna_px $2-dyna_py $3-TA*.z $4-dist $5-lon $6-lat $7-azi $8-lon $9-lat $10-Ax $11-Ay $12-Bx $13-By $14-lat $15-lon $16 Exx(lap_Ax) $17-Eyy(lap_Ay)  make sure the lon and lat are the same for each line
-paste dyna_pxpy ../header_all AB_all lap_A | awk -v w=$omega '{print $5,$6,$8,$9,$15,$14, w/sqrt((w*$1)^2-$10^2-$16+(w*$2)^2-$11^2-$17)}' > stru_velo
+# $1-dyna_px $2-dyna_py $3-lon $4-lat $5-Ax $6-Ay $7-Bx $8-By $9-lat $10-lon $11 Exx(lap_Ax) $12-Eyy(lap_Ay)  make sure the lon and lat are the same for each line
+paste dyna_pxpy AB_all lap_A | awk -v w=$omega '{print $3,$4,w/sqrt((w*$1)^2-$5^2-$11+(w*$2)^2-$6^2-$12)}' > stru_velo
